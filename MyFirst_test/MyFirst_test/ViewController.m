16e04e549398f593a5e90a56bc43d57d0fd26620
yangcom01 <893690698@qq.com>
2015-07-31 18:57:41 +0800

    my test for yang_test01

--- MyFirst_test/MyFirst_test/ViewController.m
+++ MyFirst_test/MyFirst_test/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"I am myfirstTest01");
+    NSLog(@"I am myfirstTest02");
      self.view.backgroundColor=[UIColor redColor];
 }
 - (IBAction)tapButton:(id)sender {
